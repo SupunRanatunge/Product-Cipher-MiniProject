319a8339224cd7dfa2b6f4462520dcb7b8269830
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2018-05-07 15:45:50 +0530

    Basic done

--- src/Cipher/Package/CipherMethods.java
+++ src/Cipher/Package/CipherMethods.java
@@ -6,7 +6,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -21,16 +25,23 @@ public class CipherMethods {
     int k;
     int m;
     int x;
+    int c;
+    int r;
     int numb;
     int number;
     String test="";
     String out ="";
+    String result="";
+    String result2="";
     ArrayList<String> content = new ArrayList();
     ArrayList<Character> output = new ArrayList();
     ArrayList<Character> decSub = new ArrayList();
+    char letters[][];
+    String resultArray[][];
     
     
     public CipherMethods(){
+        
         
     }
     public ArrayList substitute_txt(File file,int key) throws FileNotFoundException, IOException{
@@ -64,30 +75,57 @@ public class CipherMethods {
             }
         
         }
-        System.out.println("Encrypted content:    "+test);
-        System.out.println("Encrypted content:    "+output);
+        System.out.println("Substituted content:    "+test);
+        System.out.println("Substituted content:    "+output);
         
         
-        PrintWriter writer = new PrintWriter("Encrypted.txt", "UTF-8");
-        writer.println(test);
         
-        writer.close();
         return output;
     }
     
-    public void permutation_txt(ArrayList output,int key){
+    public void permutation_txt(ArrayList output,int key) throws FileNotFoundException{
         int rest = (key-(output.size()%key));
         if (output.size()%key != 0) {
             for (x=0;x<rest;x++){
                 output.add('x');
-                System.out.println(output);
-                
             }
-            
-            
-              
         }
+            
+            int hg = -1;
+            letters = new char[output.size()/key][key];
+            for (int v=0; v<(output.size()/key); v++) {
+                
+                for (int z=0; z<key; z++) {
+
+                    hg = hg+1;
+                    letters[v][z] =  (char) output.get(hg);
+                    
+
+                }
+            }
+            System.out.println(Arrays.deepToString(letters));
+            for (int z=0; z<key; z++) {
+                for (int v=0; v<(output.size()/key); v++) {
+                    
+                    char lk = (letters[v][z]);
+                    String.valueOf(lk);
+                    result = result + lk ;
+                }
+            
+            }
+            System.out.println("Encrypted Content :"+result);
+            PrintWriter writer = null;
+            try {
+                writer = new PrintWriter("Encrypted.txt", "UTF-8");
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(CipherMethods.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            writer.println(result);
+        
+            writer.close();
+        
     }
+    
     
     public void dec_substitute_txt(String line,int key){
         
@@ -112,6 +150,35 @@ public class CipherMethods {
         }
         System.out.println("Decrypted text:    "+ out);
     }
+    
+    
+public String de_permutation_txt(String line, int key) {
+    resultArray = new String[line.length()/key][key];
+    int fg = -1;
+    System.out.println(line+" "+ key);
+    for (int z=0; z<key; z++) {
+        for (int v=0; v<(line.length()/key); v++) {
+                
+        
+            fg = fg+1;
+            
+            resultArray[v][z] =  Character.toString(line.charAt(fg));
+        }
+    }
+    System.out.println(Arrays.deepToString(resultArray));
+    for (int v=0; v<(line.length()/key); v++) {
+        for (int z=0; z<key; z++) {
+             String tr = (resultArray[v][z]);
+             
+             result2 = result2 + tr ;
+            
+        }
+    }
+    System.out.println(result2);
+    
+    return result2;
 }
+}
+
 
 
